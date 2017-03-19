b307ed7e3cf911bdb9ad3c70c5ea8ce12f2c8e1a
Sebastian <sebastian.callh@gmail.com>
2017-03-19 11:07:01 +0100

    everything working except correct data sanitazion. Everything is also in the same file

--- main.hs
+++ main.hs
@@ -1,11 +1,15 @@
+import qualified Data.Vector as V
+import Data.Vector ((!))
 import qualified Data.Map as Map
 import Data.List.Split
 import Text.Read
 import Data.List
 
+type Grid = V.Vector (V.Vector Symbol)
+type Row = V.Vector Symbol
 type Pos = (Int, Int)
 data Board = Board { size :: Int
-                   , fields :: Map.Map Pos Symbol
+                   , tiles :: Grid
                    }
 
 data Symbol = O | X | Empty
@@ -16,50 +20,56 @@ instance Show Symbol where
   show X = "X"
   show Empty = " "
 
+-- Assumes square matrix
+transposeV :: V.Vector (V.Vector a) -> V.Vector (V.Vector a)
+transposeV v
+  | V.null $ V.head v = V.empty
+  | otherwise = V.cons (fmap V.head v) $ transposeV $ fmap (V.drop 1) v
+
 printState :: Board-> Symbol -> IO ()
 printState board symbol = do
   putStr output where
-    output = turnText ++ line ++ boardState ++ line ++ possibleMoves
+    output = turnText ++ boardState ++ possibleMoves
     turnText = "It is " ++ show symbol ++ "'s turn\n"
-    line = replicate 5 '-' ++ "\n"
-    boardState = foldr1 (++) $ format . chunksOf (size board) $ symbols
-    symbols = map (show . snd) $ Map.toList $ fields board
-    format = concatMap (\r -> "|" : r ++ ["|\n"])
-    possibleMoves = (show $ Map.keys $ emptyFields board) ++ "\n"
+    boardState = concatMap (++"\n") rows
+    rows = fmap show $ tiles board
+    possibleMoves = (show $ emptyTiles board) ++ "\n"
 
 putSymbol :: Board -> Symbol -> Pos -> Board
-putSymbol board symbol pos = Board (size board) newFields where
-  newFields = Map.adjust (\_ -> symbol) pos $ fields board
+putSymbol board symbol (rowI, colI) = Board (size board) tiles' where 
+  tiles' = V.update boardTiles (V.singleton (colI, col'))
+  col' = V.update (boardTiles ! colI) (V.singleton (rowI, symbol))
+  boardTiles = tiles board
 
 isWinner ::  Board -> Symbol -> Bool
 isWinner board symbol =
   isVerticalVictory board symbol ||
-  isHorizontalVictory symbols ||
-  isDiagonalVictory symbols where
-  symbols = Map.filter (== symbol) $ fields board
+  isHorizontalVictory board symbol ||
+  isDiagonalVictory board symbol
 
 isVerticalVictory :: Board -> Symbol -> Bool
-isVerticalVictory board symbol = any (isRowVictory board symbol) [1..size board]
+isVerticalVictory board symbol = V.any (isRowVictory symbol) (transposeV (tiles board))
 
-isRowVictory board symbol row = all isSameSymbol [1..size board] where
-  isSameSymbol col = Map.lookup (col, row) (fields board) == Just symbol
-  
-isHorizontalVictory :: Map.Map Pos Symbol -> Bool
-isHorizontalVictory symbols = False
+isHorizontalVictory :: Board -> Symbol -> Bool 
+isHorizontalVictory board symbol = V.any (isRowVictory symbol) (tiles board)
 
-isDiagonalVictory :: Map.Map Pos Symbol -> Bool
-isDiagonalVictory symbols = False
+isDiagonalVictory :: Board -> Symbol -> Bool 
+isDiagonalVictory board symbol =
+  all (== symbol) [ts ! x ! (s' - x) | x <- [0..s']] ||
+  all (== symbol) [ts ! x ! x | x <- [0..s']] where
+  ts = tiles board
+  s' = size board - 1
+
+isRowVictory :: Symbol -> Row-> Bool 
+isRowVictory symbol row = all (== symbol) row
 
 takeTurn :: Board -> Symbol -> IO Board
 takeTurn board symbol = do
   putStrLn "Enter where you want to place your symbol on the format 'x y'"
-  pos <- getPlayerMove
-  case pos of
+  input <- getPlayerMove
+  case input of
+    Just pos -> return $ putSymbol board symbol pos
     Nothing -> takeTurn board symbol
-    Just pos ->
-      if pos `Map.member` (emptyFields board)
-      then return $ putSymbol board symbol pos
-      else takeTurn board symbol
 
 -- Assumes at least 2 words as input 
 getPlayerMove :: IO (Maybe Pos)
@@ -69,31 +79,39 @@ getPlayerMove = do
     (Just x, Just y) -> return $ Just (x, y)
     _ -> return Nothing
 
-emptyFields :: Board -> Map.Map Pos Symbol
-emptyFields = Map.filter (== Empty) . fields 
+emptyTiles :: Board -> V.Vector (V.Vector Int)
+emptyTiles = fmap (V.elemIndices Empty) . tiles
 
 getWinner :: Board -> Maybe Symbol
 getWinner board 
   | isWinner board O = Just O
   | isWinner board X = Just X
   | otherwise = Nothing
-  
-play :: Board -> IO ()
+
+victory :: Board -> Symbol -> IO (Symbol)
+victory board symbol = do
+  printState board Empty
+  putStrLn (show symbol ++ " is winner!")
+  return symbol
+
+play :: Board -> IO (Symbol)
 play board = do
   printState board O
-  let winner = getWinner board
-  case winner of 
-    Just s -> putStrLn (show s ++ " is winner!")
+  board' <- takeTurn board O
+  case getWinner board' of 
+    Just s -> victory board' s 
     Nothing -> do
-      board' <- takeTurn board O
       printState board' X
       board'' <- takeTurn board' X
-      play board''
+      case getWinner board'' of
+        Just s -> victory board'' s
+        Nothing -> do
+          play board''
 
-main :: IO ()
+main :: IO (Symbol)
 main = do
-  let board = Board size fields where
+  let board = Board size tiles where
         size = 3
-        fields = Map.fromList [(pos, Empty) | pos <- positions]
-        positions = [(x, y) | x <- [1..size], y <- [1..size]]
+        tiles = V.fromList [col | _ <- [1..size]]
+        col = V.fromList [Empty | _ <- [1..size]]
   play board
